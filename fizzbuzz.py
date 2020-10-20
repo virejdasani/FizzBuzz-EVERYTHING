e46b70a9e90a48002e5a5b96e52e4ee8c652871e
virejdasani <dasanivirej@gmail.com>
2020-10-20 16:33:31 +0530

    Initial Commit

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,7 +6,6 @@ ip = input("Enter the max number to go for FizzBuzz: \n")
 # If user inputs a number this is executed
 if (ip != ""):
     for i in range(1, int(ip) + 1):
-        checker = i
         if (i % 3 == 0 and i%5 == 0):
             print("FizzBuzz")
         elif (i % 3 == 0):
@@ -19,7 +18,6 @@ if (ip != ""):
 # If the input is empty, the value is taken as 100
 else:
     for i in range(1, 101):
-        checker = i
         if (i % 3 == 0 and i%5 == 0):
             print("FizzBuzz")
         elif (i % 3 == 0):
