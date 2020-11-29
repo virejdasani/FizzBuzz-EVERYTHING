c506ceb92fa91183ff73bec01e5202429cce13a2
dillir07 <dillir07@outlook.com>
2020-11-29 23:17:09 +0530

    ts version

---
+++ Typescript/fizzbuzz.js
@@ -0,0 +1,12 @@
+var limit = 100;
+for (var counter = 0; counter <= limit; counter++) {
+    if (counter % 3 == 0 && counter % 5 == 0) {
+        console.log("FizzBuzz");
+    }
+    else if (counter % 3 == 0) {
+        console.log("Fizz");
+    }
+    else if (counter % 5 == 0) {
+        console.log("Buzz");
+    }
+}
