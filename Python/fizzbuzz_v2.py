a30ea2fc26c6439980d8b1d43b31d2d16f32f3b3
Radu-Alexandru B <radualexandrubulai@gmail.com>
2020-11-29 18:51:17 +0200

    Add fizzbuzz_v2.py
    Create an alternative FizzBuzz solution in Python

---
+++ Python/fizzbuzz_v2.py
@@ -0,0 +1,13 @@
+def FizzBuzz(num):
+    for _ in range(1, num + 1):
+        out = ""
+        if _ % 3 == 0:
+            out += "Fizz"
+        if _ % 5 == 0:
+            out += "Buzz"
+        if out == "":
+            out = _
+        print(out)
+
+
+FizzBuzz(18)
