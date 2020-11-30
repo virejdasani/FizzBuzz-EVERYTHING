01799b2cf012f0f1158a4eb7d6ce4110f69d38c7
dillir07 <dillir07@outlook.com>
2020-11-30 17:20:48 +0530

    added julia

---
+++ julia/fizzbuzz.jl
@@ -0,0 +1,17 @@
+"""
+FizzBuzz
+"""
+
+limit = 100
+
+for counter in 0:1:100;
+
+    if counter % 3 == 0 && counter % 5 == 0
+        println("FizzBuzz")
+    elseif counter % 3 == 0
+        println("Fizz")
+    elseif counter % 5 == 0
+        println("Buzz")
+    end
+end
+
