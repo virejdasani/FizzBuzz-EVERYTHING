233b66d720ba9f2e4b1c9116234642a039cd4584
Paolo Cosentino <pcosentino95@gmail.com>
2020-11-30 02:40:29 -0800

    Add Lua

--- Lua/fizzbuzz.lua
+++ Lua/fizzbuzz.lua
@@ -1,4 +1,8 @@
-for i = 1, 100 do
+print("How far do you want to go with FizzBuzz:")
+n = io.read("*n")
+
+
+for i = 1, n do
     if i % 15 == 0 then
         print("FizzBuzz")
     elseif i % 3 == 0 then
